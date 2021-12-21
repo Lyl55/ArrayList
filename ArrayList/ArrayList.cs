51f4e02bd54b2362388a58377464f6cd373c8a02
Lyl55 <88458345+Lyl55@users.noreply.github.com>
2021-12-22 02:06:44 +0400

    Add project files.

---
+++ ArrayList/ArrayList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList
+{
+    public class ArrayList
+    {
+        private object[] arr;
+        private int count = 0;
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return arr.Length;
+            }
+        }
+
+        public ArrayList()
+        {
+            arr = new object[1];
+        }
+
+        public void Add(object x)
+        {
+            if (count == arr.Length)
+            {
+                object[] arr2 = new object[arr.Length * 2];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr2[i] = arr[i];
+
+                }
+
+                arr = arr2;
+
+            }
+            arr[count++] = x;
+        }
+
+
+        public void Show()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
---
+++ ArrayList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList l = new ArrayList();
+            l.Add(9);
+            l.Add(3);
+            l.Add("array");
+            l.Add(7);
+            l.Show();
+        }
+    }
+}
